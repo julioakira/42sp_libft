437cd0d91058d1a59bc7c2baecc3d359bbbf0a01
julioakira <julio.akira.pedroso@gmail.com>
2021-07-30 02:41:20 -0300

    adds strstr file

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: jakira-p <jakira-p@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/29 03:48:40 by jakira-p          #+#    #+#             */
-/*   Updated: 2021/07/29 05:07:40 by jakira-p         ###   ########.fr       */
+/*   Updated: 2021/07/30 02:40:58 by jakira-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,22 @@
 // Otherwise, a pointer to the first characters of the
 // first occurence of needle is returned
 
-// Needs to change L28 to match const and norminette
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
+	size_t	idx;
 	size_t	n_len;
 
 	n_len = ft_strlen(needle);
 	if (!len || !n_len)
 		return ((char *) haystack);
-	while ((haystack = ft_strchr(haystack, needle[0])))
+	idx = 0;
+	while (idx <= len - n_len)
 	{
-		if (!ft_strncmp(haystack, needle, len))
-			return ((char *) haystack);
+		if ((*haystack == *needle)
+			&& (!ft_strncmp(haystack, needle, n_len)))
+			return ((char *)haystack);
 		haystack++;
+		idx++;
 	}
 	return (NULL);
 }
